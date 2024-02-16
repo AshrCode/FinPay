2c14bc5f98f34777f1e7a58939613e971b67afb1
Khizr <khizerashrafi@gmail.com>
2024-02-16 17:45:06 +0400

    Initial structure and setup

---
+++ src/Common/ApiException/ApiErrorCodes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ApiException
+{
+    public static class ApiErrorCodes
+    {
+        public const int None = 0;
+        public const int BadRequest = 400;
+        public const int Unauthorized = 401;
+        public const int Forbidden = 403;
+        public const int NotFound = 404;
+        public const int Conflict = 409;
+        public const int InternalError = 500;
+    }
+}
